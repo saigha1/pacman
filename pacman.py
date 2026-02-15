7a06597605703ea19ba2a146b75a9137b49a1226
satak <saigham@mcmaster.ca>
2026-02-14 20:13:14 -0500

    Added pacman and movement for pacman using polling (long-press)

--- pacman.py
+++ pacman.py
@@ -21,6 +21,27 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+
+        
+    #go right
+    if keys[pygame.K_RIGHT]:
+        player_x += 1
+
+    #go left
+    if keys[pygame.K_LEFT]:
+        player_x -=  1
+
+    #go up
+    if keys[pygame.K_UP]:
+        player_y -=  1
+
+    #go down
+    if keys[pygame.K_DOWN]:
+        player_y += 1
+
+
+    #Iterates through the screen to draw the board
     for r, row in enumerate(board):
         for c, character in enumerate(row):
             if character == 'W':
@@ -31,6 +52,10 @@ while running:
                 pygame.draw.rect(screen, (0, 0, 255), (x_pixel, y_pixel, TILE_SIZE, TILE_SIZE))
 
                 
+    #Draw a circle
+    pygame.draw.circle(screen, (255,255,0), (player_x,player_y), 10)
+
+
 
 
 
